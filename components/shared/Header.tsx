c7b4c4ab529f34877167d1b2bf002e0e4f7ed4fc
Erielí Carolina Lara <erieli.larad@gmail.com>
2024-01-24 18:45:17 -0500

    First part of evently

---
+++ components/shared/Header.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../../public/assets/images/logo.svg";
+import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src={logo} width={128} height={38} alt="Evently logo" />
+        </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
